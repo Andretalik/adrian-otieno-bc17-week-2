ec082eb7691f55c5a04352bc54cd31a3439a4db6
Adrian Andre <adrianadero.otieno@gmail.com>
2017-04-11 16:08:38 +0300

    Added tests and functionality for assigning rooms to created people

--- Dojo_class.py
+++ Dojo_class.py
@@ -14,39 +14,59 @@ class Dojo(object):
 
     def create_room(self, room_type, *args):
         if len(args) != 0:
-            # print("hello")
+            # print("hello") # debugging checks for correct logic
             if room_type == "office":
-                # print("world")
+                # print("world") # debugging checks for correct logic
                 for room_name in list(args):
                     office_object = Office("office", room_name)
                     self.all_rooms.append(office_object)
                     self.offices.append(office_object)
+                    # print("office created")
             elif room_type == "livingspace":
-                # print("stuff")
+                # print("stuff")# debugging checks for correct logic
                 for room_name in list(args):
-                    # print("loops")
+                    # print("loops") # debugging checks for correct logic
                     livingspace_object = LivingSpace("livingspace", room_name)
                     self.all_rooms.append(livingspace_object)
-                    self.offices.append(livingspace_object)
+                    self.livingspaces.append(livingspace_object)
+                    # print(len(self.livingspaces))
+                    # print("livingspaces") # debugging checks for correct logic
             else:
-                return "Please enter the office type in the correct format"
+                return "Please enter the room type in the correct format"
 
-    def add_person(self, person_name, person_type, wants_accomodation):
+    def add_person(self, person_name, person_type, wants_accomodation="N"):
         if person_name != "":
-            if person_type == "fellow":
-                person_object = Fellow(person_name, "fellow", wants_accommodation)
+            if person_type == "fellow": #
+                # print("a fellow") # debugging checks for correct logic
+                person_object = Fellow(person_name, person_type, wants_accomodation) # creates
                 self.all_people.append(person_object)
                 self.fellows.append(person_object)
+                # person_object.office_assigned = random.choice(self.offices)
+                if wants_accomodation == "Y":
+                    # print("room assigned") # debugging checks for correct logic
+                    #if len(self.livingspaces) != 0:
+                    # print(len(self.livingspaces))
+                    # person_object.room_assigned = random.choice(self.livingspaces)
+                    #else:
+                        #person_object.room_assigned = "No rooms exist"
+                    if (len(self.livingspaces)) == 0:
+                        person_object.room_assigned = "You can't be placed. No rooms exist"
+                    else:
+                        person_object.room_assigned = random.choice(self.livingspaces)
+
             elif person_type == "staff":
-                person_object = Staff(person_name, "staff", wants_accommodation)
+                # print("staff dont live here") # debugging checks for correct logic
+                person_object = Staff(person_name, person_type, wants_accomodation)
                 self.all_people.append(person_object)
                 self.staff.append(person_object)
+
             else:
                 return "Please enter the person type in the correct format"
 
-    def assign_room(self, person_name, person_type, wants_accommodation="N"):
 
 
 
-# doug = Dojo()
-# print(doug.create_room("livingspace", "drangleic"))
+
+'''doug = Dojo()
+doug.create_room("livingspace", "Drangleic")
+print(doug.all_rooms[0].room_name)'''
--- Unittests_for_Accommodation_App.py
+++ Unittests_for_Accommodation_App.py
@@ -6,16 +6,14 @@ class TestCreateRoom(unittest.TestCase):
     def test_create_one_room_successful(self):
         test_instance_one_room = Dojo()
         initial_rooms_count = len(test_instance_one_room.all_rooms)
-        drangleic_livingspace = test_instance_one_room.create_room("livingspace", "Drangleic")
-        # self.assertTrue(drangleic_livingspace)
+        test_instance_one_room.create_room("livingspace", "Drangleic")
         final_rooms_count = len(test_instance_one_room.all_rooms)
         self.assertEqual(final_rooms_count - initial_rooms_count, 1, 'The room has not been created')
 
     def test_create_multiple_rooms(self):
         test_instance_multiple_rooms = Dojo()
         initial_rooms_count = len(test_instance_multiple_rooms.all_rooms)
-        multiple_livingspace = test_instance_multiple_rooms.create_room("livingspace", "Drangleic", "Rockport", "Camden", "Halo")
-        # self.assertTrue(multiple_livingspace)
+        test_instance_multiple_rooms.create_room("livingspace", "Drangleic", "Rockport", "Camden", "Halo")
         self.assertEqual(test_instance_multiple_rooms.all_rooms[0].room_name, "Drangleic", "The rooms have not been created")
         self.assertEqual(test_instance_multiple_rooms.all_rooms[2].room_name, "Camden", "The rooms have not been created")
         final_rooms_count = len(test_instance_multiple_rooms.all_rooms)
@@ -23,32 +21,36 @@ class TestCreateRoom(unittest.TestCase):
 
 
 class TestAddPerson(unittest.TestCase):
+    def setUp(self):
+        self.test_case_instance = Dojo()
+
     def test_add_person_fellow_successful(self):
         test_instance_person_add = Dojo()
         initial_person_count = len(test_instance_person_add.all_people)
-        adrian_otieno_fellow = test_instance_person_add.add_person("Adrian Otieno", "fellow", "Y")
-        # self.assertTrue(adrian_otieno_fellow)
+        test_instance_person_add.add_person("Adrian Otieno", "fellow", "Y")
         final_person_count = len(test_instance_person_add.all_people)
         self.assertEqual(final_person_count - initial_person_count, 1, 'The person has not been added')
+
 
     def test_add_person_staff_successful(self):
         test_instance_person_add_staff = Dojo()
         initial_person_count = len(test_instance_person_add_staff.all_people)
-        master_chief_fellow = test_instance_person_add_staff.add_person("Master Chief", "staff", "Y")
-        # self.assertTrue(adrian_otieno_fellow)
+        test_instance_person_add_staff.add_person("Master Chief", "staff", "Y")
         final_person_count = len(test_instance_person_add_staff.all_people)
         self.assertEqual(final_person_count - initial_person_count, 1, 'The person has not been added')
 
-    '''def test_person_accommodation(self):
-        room_instance = Room("livingspace", "Drangleic", "Rockport", "Camden", "Halo")
-        multiple_livingspace = room_instance.create_room("livingspace", "Drangleic", "Rockport", "Camden", "Halo")
-        self.assertTrue(multiple_livingspace)
-        person_instance = Person("Master Chief", "Fellow", "Y")
-        master_chief_fellow = person_instance.add_person("Master Chief", "Fellow", "Y")
-        self.assertTrue(isinstance(master_chief_fellow.room_assigned, master_chief_fellow.livingspaces),
-                        msg="Person has been assigned room that does not exist")'''
-
-
-
-
-
+    def test_person_accommodation(self):
+        # test_case_instance = Dojo()
+        # multiple_livingspace = room_instance.create_room("livingspace", "Drangleic", "Rockport", "Camden", "Halo")
+        self.test_case_instance.create_room("livingspace", "Drangleic")
+        # print(test_room_assigned.all_rooms[0].room_name)
+        # print(test_room_assigned.livingspaces[0].room_name)
+        # self.assertTrue(multiple_livingspace)
+        self.test_case_instance.add_person("Master Chief", "fellow", "Y")
+        # print(person_instance.all_people[0].person_name)
+        # print(type(person_instance))
+        # self.assertTrue(isinstance(master_chief_fellow.room_assigned, master_chief_fellow.livingspaces), msg=
+        # self.assertIn(person_instance.room_assigned, person_instance.livingspaces, msg="Person has been assigned a room that does not exist.")
+        # print(test_case_instance.livingspaces[0].room_name)
+        # print(test_case_instance.all_people[0].room_assigned)
+        self.assertEqual(self.test_case_instance.all_people[0].room_assigned.room_name, self.test_case_instance.all_rooms[0].room_name, "Person hasn't been assigned a room")
