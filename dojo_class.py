60164244f6d10722706748c27bb237b650b7703e
Andretalik <adrianadero.otieno@gmail.com>
2017-05-29 21:47:23 +0300

    Clean up code
     - Removed commented out code

--- dojo_class.py
+++ dojo_class.py
@@ -6,30 +6,24 @@ from person_class import Fellow, Staff
 class Dojo(object):
     def __init__(self):
         self.all_rooms = []
-        # self.livingspaces = []
-        # self.offices = []
         self.all_people = []
-        # self.fellows = []
-        # self.staff = []
         self.available_offices = []
         self.available_livingspaces = []
         self.unallocated_offices = []
         self.unallocated_livingspaces = []
 
     def create_room(self, room_type, rooms_to_make):
-        """This creates an office or livngspace for use in the system"""
+        """This creates an office or livingspace for use in the system"""
         if len(rooms_to_make) != 0:
             for room_name in rooms_to_make:
                 if room_type == "office":
                     office_instance = Office("office", room_name)
                     self.all_rooms.append(office_instance)
-                    # self.offices.append(office_instance)
                     self.available_offices.append(office_instance)
                     print("An {} called {} has been created".format(office_instance.room_type, office_instance.room_name))
                 elif room_type == "livingspace":
                     livingspace_instance = LivingSpace("livingspace", room_name)
                     self.all_rooms.append(livingspace_instance)
-                    # self.livingspaces.append(livingspace_instance)
                     self.available_livingspaces.append(livingspace_instance)
                     print("A {} called {} has been created".format(livingspace_instance.room_type, livingspace_instance.room_name))
                 else:
@@ -43,58 +37,62 @@ class Dojo(object):
                 fellow_instance = Fellow(person_first_name, person_second_name, person_type, wants_accomodation)
                 self.all_people.append(fellow_instance)
                 print("A {} called {} has been created".format(fellow_instance.person_type, fellow_instance.person_first_name))
-                self.allocate_livingspace
+                self.allocate_livingspace()
             elif person_type == "staff":
-                staff_instance = Staff(person_first_name, person_second_name, person_type, wants_accomodation)
+                staff_instance = Staff(person_first_name, person_second_name, person_type)
                 self.all_people.append(staff_instance)
                 print("A {} member called {} has been created".format(staff_instance.person_type, staff_instance.person_first_name))
-            self.allocate_office
-        self.room_checker
+            else:
+                return "Please enter the person type in the correct format."
+            self.allocate_office()
+            self.room_checker()
         else:
-            return "Please enter the person type in the correct format."
-
+            return "Please enter the person's details in the correct format."
 
     def allocate_office(self):
-        if person_type == "fellow":
-            if (len(self.available_offices)) == 0:
-                self.unallocated_offices.append(fellow_instance)
-                print("{} has been added to the waiting list due to no available rooms".format(fellow_instance.person_first_name))
-            else:
-                chosen_office_object = random.choice(self.available_offices)
-                fellow_instance.office_assigned = chosen_office_object
-                chosen_office_object.occupants.append(fellow_instance)
-                print("{} has been allocated office {}".format(fellow_instance.person_first_name, fellow_instance.office_assigned.room_name))
-        elif person_type == "staff":
-            if (len(self.available_offices)) == 0:
-                self.unallocated_offices.append(staff_instance)
-                print("{} has been added to the waiting list due to no available rooms".format(staff_instance.person_first_name))
-            else:
-                chosen_office_object = random.choice(self.available_offices)
-                staff_instance.office_assigned = chosen_office_object
-                chosen_office_object.occupants.append(staff_instance)
-                print("{} has been allocated office {}".format(staff_instance.person_first_name, staff_instance.office_assigned.room_name))
+        for person in self.all_people:
+            if person.person_type == "fellow":
+                if (len(self.available_offices)) == 0:
+                    self.unallocated_offices.append(person)
+                    print("{} has been added to the waiting list due to no available rooms".format(person.person_first_name))
+                else:
+                    chosen_office_object = random.choice(self.available_offices)
+                    person.office_assigned = chosen_office_object
+                    chosen_office_object.occupants.append(person)
+                    print("{} has been allocated office {}".format(person.person_first_name, person.office_assigned.room_name))
+            elif person.person_type == "staff":
+                if (len(self.available_offices)) == 0:
+                    self.unallocated_offices.append(person)
+                    print("{} has been added to the waiting list due to no available rooms".format(person.person_first_name))
+                else:
+                    chosen_office_object = random.choice(self.available_offices)
+                    person.office_assigned = chosen_office_object
+                    chosen_office_object.occupants.append(person)
+                    print("{} has been allocated office {}".format(person.person_first_name, person.office_assigned.room_name))
 
     def allocate_livingspace(self):
-        if wants_accomodation == "Y":
-            if (len(self.available_livingspaces)) == 0:
-                self.unallocated_livingspaces.append(fellow_instance)
-            else:
-                chosen_livingspace_object = random.choice(self.available_livingspaces)
-                fellow_instance.livingspace_assigned = chosen_livingspace_object
-                chosen_livingspace_object.occupants.append(fellow_instance)
-                print("{} has been allocated living space {}".format(fellow_instance.person_first_name, fellow_instance.livingspace_assigned.room_name))
+        for person in self.all_people:
+            if isinstance(person, Fellow):
+                if person.wants_accommodation == "Y":
+                    if (len(self.available_livingspaces)) == 0:
+                        self.unallocated_livingspaces.append(person)
+                    else:
+                        chosen_livingspace_object = random.choice(self.available_livingspaces)
+                        person.livingspace_assigned = chosen_livingspace_object
+                        chosen_livingspace_object.occupants.append(person)
+                        print("{} has been allocated living space {}".format(person.person_first_name, person.livingspace_assigned.room_name))
 
     def room_checker(self):
         for office in self.available_offices:
             if len(office.occupants) < office.max_no:
                 pass
             else:
-                self.available_offices.pop(office)
+                self.available_offices.remove(office)
         for livingspace in self.available_livingspaces:
             if len(livingspace.occupants) < livingspace.max_no:
                 pass
             else:
-                self.available_livingspaces.pop(livingspace)
+                self.available_livingspaces.remove(livingspace)
 
     def print_room(self, room_name):
         """This prints all the people who have been alllocated a specific room by
@@ -127,3 +125,24 @@ class Dojo(object):
                 output_txt_file.write(allocation_output)
                 output_txt_file.close()
                 print("The allocation data has been written to " + option_to_txt_file)
+
+    def print_unallocated(self, option_to_txt_file=""):
+        """This prints out to the console or to a text file all the
+            unallocated people within the system"""
+        unallocated_output = ""
+        if len(self.all_people) == 0:
+            return "No unallocated people, because there are no people in the system."
+        else:
+            if len(self.unallocated_offices) == 0 and len(self.unallocated_livingspaces) == 0:
+                return "Everyone has been allocated an office and livingspace. Yay!"
+            else:
+                unallocated_output += ("\n\n Unallocated Offices")
+                for person in self.unallocated_offices:
+                    unallocated_output += (person.person_first_name + " " + person.person_second_name)
+                if option_to_txt_file == "":
+                    print(unallocated_output)
+                else:
+                    txt_file_unallocated = open(option_to_txt_file + ".txt", "w+")
+                    txt_file_unallocated.write(unallocated_output)
+                    txt_file_unallocated.close()
+                    return "The data concerning unallocated people has been written to {}".format(option_to_txt_file)
